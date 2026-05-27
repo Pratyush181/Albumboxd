5006bdc26d0db06b58d77927c16c04b9b0cbdac3
Pratyush <pratyushshankar18@gmail.com>
2026-05-27 21:54:01 +0530

    changes to index

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 const express = require('express');
-const app=express();
+const app = express();
 const cors = require("cors");
 const bcrypt = require('bcrypt');
 const corsOptions = {
-    origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173"],
 };
 const User = require("./models/User");
 const mongoose = require("mongoose");
-const mongoURI = process.env.MONGO_URI || "mongodb+srv://Pratyush:pratmongopassword@users.axtqgnr.mongodb.net/?retryWrites=true&w=majority&appName=Users";
+const mongoURI = process.env.MONGO_URI;
 const spotifyRoutes = require("./routes/spotify");
 const Album = require('./models/Album');
 const ratingRoutes = require('./routes/ratings');
@@ -21,7 +21,7 @@ const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 // connect to MongoDB
 mongoose.connect(mongoURI, {
-  })
+})
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch(err => console.error("❌ MongoDB connection error:", err));
 
@@ -36,18 +36,18 @@ app.use(express.urlencoded({ extended: true })); // This parses URL-encoded bodi
 
 //Test User
 app.post('/test-user', async (req, res) => {
-    try {
-        const testUser = new User({
-            username: "testuser",
-            email: "prat@example.com",
-            password: "pratletsgo1818"
-        });
+  try {
+    const testUser = new User({
+      username: "testuser",
+      email: "prat@example.com",
+      password: "pratletsgo1818"
+    });
 
-        const savedUser = await testUser.save();
-        res.json(savedUser);
-    } catch (error) {
-        res.status(400).json({error: "Error creating test user", details: error.message});
-    }    
+    const savedUser = await testUser.save();
+    res.json(savedUser);
+  } catch (error) {
+    res.status(400).json({ error: "Error creating test user", details: error.message });
+  }
 });
 
 
@@ -166,9 +166,9 @@ app.post('/api/signup', async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    const existingUser = await User.findOne({email});
+    const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({message:"User already exists"})
+      return res.status(400).json({ message: "User already exists" })
     }
 
     const newUser = new User({
@@ -178,12 +178,12 @@ app.post('/api/signup', async (req, res) => {
     });
 
     await newUser.save();
-    res.status(201).json({ 
-      message: 'User created successfully', 
+    res.status(201).json({
+      message: 'User created successfully',
       user: { _id: newUser._id, username, email }
     });
-  } catch (error){
-    res.status(500).json({ message:'Server error', error: error.message });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
   }
 });
 
@@ -243,7 +243,7 @@ app.post('/test-album', async (req, res) => {
       artist: 'Pitbull',
       imageUrl: 'https://example.com/image.jpg'
     });
-    
+
     const savedAlbum = await testAlbum.save();
     res.json(savedAlbum);
   } catch (error) {
@@ -270,7 +270,7 @@ app.get('/users', async (req, res) => {
   }
 });
 
-app.listen(3000, () =>{
-    console.log("Server is running on port 3000");
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
 
